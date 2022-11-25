0d94e0a1d1f499abddd5d515069b229461ed5119
Florian Calabrese florian.calabrese@gmail.com <florian.calabrese@gmail.com>
2022-11-25 15:37:09 +0100

    mis à jour du footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_11_25_133640) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_145134) do
->>>>>>> 89c4360 (wip)
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,9 +68,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_145134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "model"
-    t.string "origin"
     t.float "latitude"
     t.float "longitude"
+    t.string "origin"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
